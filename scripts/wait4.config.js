5bcb7d73e746c0d5dd121e32f19ccfdc9ab18426
Kevin <kleyow@gmail.com>
2021-09-16 20:36:36 -0500

    chore: fix wait4

--- scripts/wait4.config.js
+++ scripts/wait4.config.js
@@ -49,6 +49,12 @@ module.exports = {
           uri: 'localhost:3008',
           method: 'ncat',
           retries: 30
+        },
+        {
+          description: 'central-ledger',
+          uri: 'localhost:3001',
+          method: 'ncat',
+          retries: 30
         }
       ]
     }
